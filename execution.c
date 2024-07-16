82b2179a0eb32c73f63da8e635209e73cb74dc60
Sheldon Chong <shechong@u81z09s02.42kl.edu.my>
2024-07-16 18:40:01 +0800

    first piping prototype working

--- execution.c
+++ execution.c
@@ -28,35 +28,112 @@ char	*get_path(char *cmd, char **env)
 	return ("\0");
 }
 
-// executes a given command and its following parameters
-void run_cmd(char **cmd, char **env, t_token_info *token_info)
+int	bash_cmd(char **env, t_token_info *token_info)
 {
-	char	*command;
-	int		pid;
+	char	**args;
+	int		i;
+
+	args = token_info->cmd_start->tokens;
+	if ((!ft_strcmp(args[0], "echo")) && args[1] && !ft_strcmp(args[1], "-n"))
+	{
+		i = 1;
+		while (args[++i])
+			printf("%s", args[i]);
+	}
+	if (!ft_strcmp(args[0], "exit"))
+	{
+		free_TokenList(token_info);
+		exit(0);
+	}
+	if (!ft_strcmp(args[0], "cd"))
+		chdir(args[1]);
+	if (!ft_strcmp(args[0], "env"))
+		print_env(&token_info->global_env, 'e');
+	if (!ft_strcmp(args[0], "unset") && args[1])
+		unset_env(args[1], &token_info->global_env, token_info);
+	if (!ft_strcmp(args[0], "export"))
+		export(token_info);
+	return (0);
+}
+
+void run_cmd(char **cmd, char **env, t_token_info *token_info, int in_fd, int out_fd)
+{
+	char *command;
+	pid_t pid;
 
 	command = get_path(cmd[0], env);
 	pid = fork();
-	if (pid != 0)
+	if (pid != 0) // parent
 	{
-		wait(0);
+		if (in_fd != STDIN_FILENO)
+			close(in_fd);
+		if (out_fd != STDOUT_FILENO)
+			close(out_fd);
+		waitpid(pid, NULL, 0);
 		if (command[0] != '\0')
 			free(command);
 	}
-	else if (pid == 0)
+	else if (pid == 0) //child
 	{
+		if (in_fd != STDIN_FILENO)
+		{
+			dup2(in_fd, STDIN_FILENO);
+			close(in_fd);
+		}
+		if (out_fd != STDOUT_FILENO)
+		{
+			dup2(out_fd, STDOUT_FILENO);
+			close(out_fd);
+		}
 		if (access(command, F_OK) == 0)
 			execve((const char *)command, (char *const *)cmd, env);
-		exit(0);
+		perror("execve");
+		exit(EXIT_FAILURE);
 	}
 }
 
-//to be replaced by Jyap
-void	run_cmds(char **env, t_token_info *token_info)
+// Function to run a list of commands with piping
+void run_cmds(char **env, t_token_info *token_info)
 {
 	t_token *chunk_list = token_info->token_chunks;
+	int pipefd[2];
+	int in_fd = STDIN_FILENO;
+	int status;
+
 	while (chunk_list)
 	{
-		run_cmd(chunk_list->tokens, env, token_info);
+		if (chunk_list->next)
+		{
+			if (pipe(pipefd) == -1)
+			{
+				perror("pipe");
+				exit(EXIT_FAILURE);
+			}
+		}
+		else
+			pipefd[1] = STDOUT_FILENO; // Last command writes to stdout
+
+		if (str_in_arr(chunk_list->start->word, BASH_CMDS))
+		{
+			if (in_fd != STDIN_FILENO)
+				close(in_fd);
+			bash_cmd(env, token_info);
+		}
+		else
+			run_cmd(chunk_list->tokens, env, token_info, in_fd, pipefd[1]);
+
+		if (pipefd[1] != STDOUT_FILENO)
+			close(pipefd[1]);
+		in_fd = pipefd[0]; // The input for the next command is the output of the current command
 		chunk_list = chunk_list->next;
 	}
+
+	// Close the last input file descriptor if it's not STDIN
+	if (in_fd != STDIN_FILENO)
+		close(in_fd);
+
+	// Wait for all child processes to finish
+	while (wait(&status) > 0);
+
+	printf("ended\n");
 }
--- minishell.c
+++ minishell.c
@@ -1,33 +1,7 @@
 #include "minishell.h"
 
 
-int	bash_cmd(char **env, t_token_info *token_info)
-{
-	char	**args;
-	int		i;
 
-	args = token_info->cmd_start->tokens;
-	if ((!ft_strcmp(args[0], "echo")) && args[1] && !ft_strcmp(args[1], "-n"))
-	{
-		i = 1;
-		while (args[++i])
-			printf("%s", args[i]);
-	}
-	if (!ft_strcmp(args[0], "exit"))
-	{
-		free_TokenList(token_info);
-		exit(0);
-	}
-	if (!ft_strcmp(args[0], "cd"))
-		chdir(args[1]);
-	if (!ft_strcmp(args[0], "env"))
-		print_env(&token_info->global_env, 'e');
-	if (!ft_strcmp(args[0], "unset") && args[1])
-		unset_env(args[1], &token_info->global_env, token_info);
-	if (!ft_strcmp(args[0], "export"))
-		export(token_info);
-	return (0);
-}
 
 void	ctrl_c_function(int signum)
 {
@@ -85,13 +59,11 @@ int	main(int ac, char **av, char **env)
 			add_history(user_input);
 			token_info = process_input(user_input, global_env);
 			token_info->env_arr = env;
+			print_tokens(token_info, 'l');
 			if (scan_cmd(token_info) && !token_info->has_error)
 			{
-				if (str_in_arr(token_info->cmd_start->tokens[0], BASH_CMDS))
-					bash_cmd(env, token_info);
 				run_cmds(env, token_info); //THIS IS TO BE REPLACED
 			}
-			print_tokens(token_info, 'l');
 			free_TokenList(token_info);
 		}
 	}
