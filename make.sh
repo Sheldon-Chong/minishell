ceaa644ca3434cd56233aee67c12b88076472fbe
Sheldon Chong <shechong@u81z09s02.42kl.edu.my>
2024-07-16 17:53:42 +0800

    makefile added

--- make.sh
+++
@@ -1,8 +0,0 @@
-gcc *.c ./libft/*.c ./tokenization/*.c ./env/*.c -I./includes -I./libft -lncurses -lreadline -o minishell -fsanitize=address -g3 && ./minishell
-#gcc *.c ./libft/*.c ./pipex/*.c ./pipex/srcs/*.c -lncurses -lreadline  && valgrind --leak-check=full ./a.out
-
-
-# -fsanitize=address 
-# include "minishell.h" 
-# valgrind --leak-check=full 
-# -fsanitize=address -g3
