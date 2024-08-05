6085bf043cb4715c39991d06f3b40cac554d1d5e
Jien Fei Yap <jyap@u82z04s06.42kl.edu.my>
2024-08-05 10:20:00 +0800

    changed ft_unset

--- src/execution/bash_cmd/ft_unset.c
+++ src/execution/bash_cmd/ft_unset.c
@@ -6,21 +6,32 @@
 /*   By: jyap <jyap@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/05 09:17:33 by jyap              #+#    #+#             */
-/*   Updated: 2024/08/05 09:17:34 by jyap             ###   ########.fr       */
+/*   Updated: 2024/08/05 10:19:46 by jyap             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	unset_env(char **var_names, t_env **envList, t_token_info *token_info)
+void	free_env_node(t_env *node)
+{
+	t_env	*temp;
+
+	temp = node;
+	node = node->next;
+	free (temp->name);
+	free (temp->value);
+	free (temp);
+}
+
+void	unset_env_sub(char **var_names, t_env **envList,
+			t_token_info *token_info)
 {
 	t_env	*head;
 	t_env	*prev;
-	t_env	*temp;
 	int		i;
 
-	i = 0;
-	while (var_names[i])
+	i = -1;
+	while (var_names[++i])
 	{
 		head = *envList;
 		prev = NULL;
@@ -32,18 +43,18 @@ int	unset_env(char **var_names, t_env **envList, t_token_info *token_info)
 					prev->next = head->next;
 				else
 					*envList = head->next;
-				temp = head;
-				head = head->next;
-				free(temp->name);
-				free(temp->value);
-				free(temp);
+				free_env_node(head);
 				break ;
 			}
 			prev = head;
 			head = head->next;
 		}
-		i++;
 	}
+}
+
+int	unset_env(char **var_names, t_env **envList, t_token_info *token_info)
+{
+	unset_env_sub(var_names, envList, token_info);
 	ft_free_array((void **)token_info->env_data->environ_arr, 0);
 	token_info->env_data->environ_arr = env2arr(*envList);
 	return (0);
