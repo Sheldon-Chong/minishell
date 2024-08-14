ff9602bb5aeb455590ad80a1e361e26e01584c68
Sheldon Chong <shechong@u91z05s03.42kl.edu.my>
2024-08-14 16:17:00 +0800

    finished

--- run.bash
+++
@@ -1,8 +0,0 @@
-make
-valgrind --leak-check=full \
-         --show-leak-kinds=all \
-         --track-origins=yes \
-         --verbose \
-         --log-file=valgrind-out.txt \
-         --suppressions=readline.supp \
-         ./bin/minishell
